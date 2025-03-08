9c6915698e25152c2d091428d25b80ef136dd7ce
lihco32 <lihco32@proton.me>
2025-03-08 02:13:32 +0200

    add support for names and lore

--- src/main/java/lihco3/scshop/scshop/config/currency/CurrencyConfig.java
+++ src/main/java/lihco3/scshop/scshop/config/currency/CurrencyConfig.java
@@ -11,24 +11,25 @@ import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class CurrencyConfig {
-    private static final FileConfiguration config;
+    private static FileConfiguration config;
 
     static {
-        config = loadCurrencyConfig();
+        loadCurrencyConfig();
     }
 
-    private static FileConfiguration loadCurrencyConfig() {
+    private static void loadCurrencyConfig() {
         final Plugin plugin = Scshop.getInstance();
         File currencyConfigFile = new File(plugin.getDataFolder(), "currency_config.yml");
 
         if(currencyConfigFile.exists()) {
-            return YamlConfiguration.loadConfiguration(currencyConfigFile);
+            config = YamlConfiguration.loadConfiguration(currencyConfigFile);
         }
 
         // Default config
-        var config = new YamlConfiguration();
+        config = new YamlConfiguration();
         setDefaultCurrencyConfig();
         try {
             config.save(currencyConfigFile);
@@ -36,13 +37,13 @@ public class CurrencyConfig {
         } catch (IOException e) {
             plugin.getLogger().warning("SCShop failed to save default config");
         }
-
-        return config;
     }
 
     private static void setDefaultCurrencyConfig() {
         config.set("currencyitem1.id", "DIAMOND");
         config.set("currencyitem1.custom_model_data", 1);
+        config.set("currencyitem1.name", "<!i><gradient:#DCE5F1:#8DA3DF>Diamond</gradient>");
+        config.set("currencyitem1.lore", List.of("<!i><gray>This is a special item</gray>", "<!i><gradient:#DCE5F1:#8DA3DF>Very valuable</gradient>"));
 
         config.set("currencyitem2.id", "COAL");
         config.set("currencyitem2.custom_model_data", 22);
@@ -59,10 +60,15 @@ public class CurrencyConfig {
         Material material = Material.matchMaterial(materialName);
         if (material == null) return null;
 
+        @Nullable String name = config.getString(currencyKey + ".name");
+        @Nullable List<String> lore = config.getStringList(currencyKey + ".lore");
+
         return new CurrencyData(
                 currencyKey,
                 materialName,
-                customModelData == 0 ? null : customModelData
+                customModelData == 0 ? null : customModelData,
+                name,
+                lore
         );
     }
 
--- src/main/java/lihco3/scshop/scshop/config/currency/CurrencyData.java
+++ src/main/java/lihco3/scshop/scshop/config/currency/CurrencyData.java
@@ -1,10 +1,12 @@
 package lihco3.scshop.scshop.config.currency;
 
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CurrencyData {
     @NotNull
@@ -16,13 +18,24 @@ public class CurrencyData {
     @Nullable
     public Integer customModelData;
 
+    @Nullable
+    public String name;
+
+    @Nullable
+    public List<String> lore;
+
     public CurrencyData(
             @NotNull String ymlId,
             @NotNull String itemId,
-            @Nullable Integer customModelData) {
+            @Nullable Integer customModelData,
+            @Nullable String name,
+            @Nullable List<String> lore
+            ) {
         this.ymlId = ymlId;
         this.itemId = itemId;
         this.customModelData = customModelData;
+        this.name = name;
+        this.lore = lore;
     }
 
     public @Nullable ItemStack getItemStack(int amount) {
@@ -35,8 +48,21 @@ public class CurrencyData {
         if (customModelData != null) {
             var meta = item.getItemMeta();
             if (meta != null) {
+                // Custom Model Data
                 meta.setCustomModelData(customModelData);
-                //meta.displayName(Component.text(""));
+                var miniMessage = MiniMessage.miniMessage();
+
+                if(name != null) {
+                    var text = miniMessage.deserialize(name);
+                    meta.displayName(text);
+                }
+
+                if(lore != null) {
+                    meta.lore(lore.stream()
+                            .map(miniMessage::deserialize)
+                            .toList());
+                }
+
                 item.setItemMeta(meta);
             }
         }
